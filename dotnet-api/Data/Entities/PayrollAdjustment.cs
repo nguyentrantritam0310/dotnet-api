a03342ad191cde5d2377130aa1d4637482e7a706
HTham090603 <hongtham123456@gmail.com>
2025-09-26 22:09:24 +0700

    thawms khumf

---
+++ dotnet-api/Data/Entities/PayrollAdjustment.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_api.Data.Enums;
+
+namespace dotnet_api.Data.Entities
+{
+    public class PayrollAdjustment
+    {
+        public string voucherNo { get; set; }
+        public DateTime decisionDate { get; set; }
+        public int Month { get; set; } // month year
+        public int Year { get; set; } // month year
+
+        public string Reason { get; set; }
+        public ApproveStatusEnum ApproveStatus { get; set; }
+     
+        // Navigation properties
+        public AdjustmentItem AdjustmentItem { get; set; }
+        public AdjustmentType AdjustmentType { get; set; }
+
+    }
+}
